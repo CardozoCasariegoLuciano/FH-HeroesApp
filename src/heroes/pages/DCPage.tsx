19bf1216a4847bad435716892e04260607d82c87
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2023-02-17 12:44:29 -0300

    init heroes app

---
+++ src/heroes/pages/DCPage.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const DCPage = () =>{
+  return( 
+    <div>
+      <h1>DCPage</h1>
+    </div>
+ )
+}
+
+export default DCPage
---
+++ src/heroes/pages/MarvelPage.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const MarvelPage = () =>{
+  return( 
+    <div>
+      <h1>MarvelPage</h1>
+    </div>
+ )
+}
+
+export default MarvelPage
